d2d4bdeeb945397d23ef8391f5e6321950ae3d69
vijaypandav700 <vijaypandav700@gmail.com>
2025-12-22 15:37:41 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,34 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
+
+// import chatRoutes from "./routes/chatRoutes.js";
+// import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
+// middleware
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+// app.use("/api/chat", chatRoutes);
+// app.use("/api/sessions", sessionRoutes);
+
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
-app.get("/books", (req, res) => {
-  res.status(200).json({ msg: "This is books endpoint" });
-});
-
-//make our app ready for deployment
+// make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
